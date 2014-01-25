e4a8111229739a69761573506e23b399f72a85a3
Marisa Mazzotta <mjmazzotta@gmail.com>
2014-01-25 15:49:58 -0500

    created the "worst" variable

--- Week3HomeworkPart7.R
+++ Week3HomeworkPart7.R
@@ -94,29 +94,103 @@ rankall <- function(outcome, num = "best") {
    #  The rank for each state and hospital is the order - the state's min. order
    outcome2.dat$rank <- outcome2.dat$order - outcome2.dat$minOrder
    
-##############################################################################  
+   
+#############################################################################   
+ # create a logical variable for "worst"
+  # first get the "worst" value for each state
+   st.max.dat <- aggregate(x=outcome2.dat$rank, 
+                             by=list(outcome2.dat$State), FUN='max')
+   names(st.max.dat)<-c('State','worst.rank')
+   # then merge this back into the data set
+   outcome2.dat <- merge(st.max.dat, outcome2.dat, by="State", all=T)
+  
+ 
+  for (i in 1:nrow(outcome2.dat)) {
+    ifelse(outcome2.dat[i,]$worst.rank==outcome2.dat[i,]$rank, 
+         outcome2.dat[i,]$worst.yes <- TRUE, 
+           outcome2.dat[i,]$worst.yes <-FALSE)
+      }
+   
+   # then create the worst.yes variable
+   outcome2.dat$worst.yes <- FALSE
+   
+############################################################################## 
+   
+   # create a 2x50 dataframe for the output
+   result <- data.frame(hospital=rep(NA, 50), state=rep(NA, 50))
+ 
+   
+   # loop over states to get the outcome for each
+   For(i in outcome2.dat$State) {
+   
+     # assign the best and worst values to num
+     ifelse(num=="best", num <- 1, num <- num)
+     ifelse(num=="worst", num <- outcome2.dat$worst.rank, num <- num)
+  
+     
+     # put data into the data frame "outfile"
+     result[i,] <- c(outcome2.dat$Hospital.Name[i],nobs)
+     
+   }
    
    
-   
-   
-   
-   ha.dat.ord <- ha.dat.ord[,-c(2,5)]
    
   
+   ## Return hospital name in that state with the given rank
+   ## using if...else for outcomes
+   if(outcome=="heart attack") {
+     result <- ha.dat[ha.dat$ha.rank==num,]
+   } else { 
+     if(outcome=="heart failure") {
+       result <- hf.dat[hf.dat$hf.rank==num,]  
+     } else { # the last option, which is pneumonia
+       result <- pn.dat[pn.dat$pn.rank==num,]
+     }
+   }
+   
+   ## Return the result    
+   ##  Return NA if num is too large  
+   ## need to do this for each outcome
+   if(outcome=="heart attack") {
+     ifelse(num > max(ha.dat$ha.rank), return(NA), return(result$Hospital.Name))
+   } else { 
+     if(outcome=="heart failure") {
+       ifelse(num > max(hf.dat$hf.rank), return(NA), return(result$Hospital.Name))
+     } else { # the last option, which is pneumonia
+       ifelse(num > max(pn.dat$pn.rank), return(NA), return(result$Hospital.Name))
+     }
+   }   
+   
+   
+  }  # end if(outcome)
+  
+  else { stop("invalid outcome") }    
+  
+}  # end rankhospital
+
+
+
+
+   
+   
+   
+   
+   
+   
+ 
+ ##############################################################################  
+  
+   #NOT SURE WHY WE DID THIS 
+   ha.dat.ord <- ha.dat.ord[,-c(2,5)] 
+   if(outcome=="heart attack") keep="Heart.Attack"
+   a<-outcome.dat[,c('State','Hospital.Name',keep)]
+   
    
    
    summary(outcome2.dat)
    summary(outcome.dat$Heart.Attack)
    
- ##############################################################################  
-   if(outcome=="heart attack") keep="Heart.Attack"
-      a<-outcome.dat[,c('State','Hospital.Name',keep)]
-   
-   
-   
-   
-   
-   #this works
+  
    
    # run through the list of states and find rank "num"
    # 1st define a function to select the relevant row for each state
@@ -126,61 +200,8 @@ rankall <- function(outcome, num = "best") {
    }
    result <- sapply(state.abb, row.select)
    
+  ##############################################################################  
    
-   
-   
-   
-   
-      # assign "best" and "worst" to ranks
-      # best is rank 1, worst is highest rank
-      # need to do this for each outcome
-      if(outcome=="heart attack") {
-        ifelse(num=="best", num <- 1, num <- num)
-        ifelse(num=="worst", num <- max(ha.dat$ha.rank), num <- num)
-      } else { 
-        if(outcome=="heart failure") {
-          ifelse(num=="best", num <- 1, num <- num)
-          ifelse(num=="worst", num <- max(hf.dat$hf.rank), num <- num)  
-        } else { # the last option, which is pneumonia
-          ifelse(num=="best", num <- 1, num <- num)
-          ifelse(num=="worst", num <- max(pn.dat$pn.rank), num <- num)  
-        }
-      }
-      
-      ## Return hospital name in that state with the given rank
-      ## using if...else for outcomes
-      if(outcome=="heart attack") {
-        result <- ha.dat[ha.dat$ha.rank==num,]
-      } else { 
-        if(outcome=="heart failure") {
-          result <- hf.dat[hf.dat$hf.rank==num,]  
-        } else { # the last option, which is pneumonia
-          result <- pn.dat[pn.dat$pn.rank==num,]
-        }
-      }
-      
-      ## Return the result    
-      ##  Return NA if num is too large  
-      ## need to do this for each outcome
-      if(outcome=="heart attack") {
-        ifelse(num > max(ha.dat$ha.rank), return(NA), return(result$Hospital.Name))
-      } else { 
-        if(outcome=="heart failure") {
-          ifelse(num > max(hf.dat$hf.rank), return(NA), return(result$Hospital.Name))
-        } else { # the last option, which is pneumonia
-          ifelse(num > max(pn.dat$pn.rank), return(NA), return(result$Hospital.Name))
-        }
-      }   
-      
-      
-    }  # end if(outcome)
-    
-    else { stop("invalid outcome") }    
-  
-}  # end rankhospital
-  
-  
-  
   
   #####################
   # For testing
@@ -188,3 +209,6 @@ rankall <- function(outcome, num = "best") {
   outcome<-"heart failure"
   outcome<-"pneumonia"
   outcome<-"HEART ATTACK"
+
+
+
